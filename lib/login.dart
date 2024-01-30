0b93dae6f6192f5189ef349c3d00bc74f083f172
Bui Duc Canh <buiduccanh10@gmail.com>
2024-01-30 15:43:21 +0700

    new file:   animations/animated_login_character.riv 	new file:   lib/login.dart 	modified:   lib/setting.dart 	new file:   lib/widget/setting/setting_content.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

---
+++ lib/login.dart
@@ -0,0 +1,238 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:money_mate/main.dart';
+import 'package:rive/rive.dart' hide LinearGradient;
+
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  final admin_email = 'admin';
+  final admin_password = 'admin';
+
+  TextEditingController email_controller = TextEditingController();
+  TextEditingController password_controller = TextEditingController();
+  FocusNode email_focus_node = FocusNode();
+  FocusNode password_focus_node = FocusNode();
+  StateMachineController? state_controller;
+  SMIInput<bool>? isChecking;
+  SMIInput<bool>? isHandsUp;
+  SMIInput<bool>? trigSuccess;
+  SMIInput<bool>? trigFail;
+  SMIInput<double>? numLook;
+
+  @override
+  void initState() {
+    email_focus_node.addListener(email_focus);
+    password_focus_node.addListener(password_focus);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    email_focus_node.removeListener(email_focus);
+    password_focus_node.removeListener(password_focus);
+    super.dispose();
+  }
+
+  void email_focus() {
+    isChecking?.change(email_focus_node.hasFocus);
+  }
+
+  void password_focus() {
+    isHandsUp?.change(password_focus_node.hasFocus);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xffD6E2EA),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 130.0, left: 14, right: 14),
+          child: Column(
+            children: [
+              SizedBox(
+                  height: 250,
+                  child: RiveAnimation.asset(
+                    'animations/animated_login_character.riv',
+                    stateMachines: ["Login Machine"],
+                    onInit: (artboard) {
+                      state_controller = StateMachineController.fromArtboard(
+                          artboard, "Login Machine");
+                      if (state_controller == null) return;
+
+                      artboard.addController(state_controller!);
+                      isChecking = state_controller?.findInput("isChecking");
+                      isHandsUp = state_controller?.findInput("isHandsUp");
+                      numLook = state_controller?.findInput("numLook");
+                      trigSuccess = state_controller?.findInput("trigSuccess");
+                      trigFail = state_controller?.findInput("trigFail");
+                    },
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          focusNode: email_focus_node,
+                          controller: email_controller,
+                          onChanged: (value) {
+                            if (isHandsUp != null) {
+                              isHandsUp!.change(false);
+                            }
+                            if (isChecking == null) {
+                              return;
+                            } else {
+                              isChecking!.change(true);
+                              numLook?.change(value.length.toDouble());
+                            }
+                          },
+                          decoration: InputDecoration(
+                              label: const Text('Email'),
+                              prefixIcon: const Icon(
+                                Icons.email,
+                              ),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          controller: password_controller,
+                          obscureText: true,
+                          focusNode: password_focus_node,
+                          keyboardType: TextInputType.visiblePassword,
+                          onChanged: (value) {
+                            if (isChecking != null) {
+                              isChecking!.change(false);
+                            }
+                            if (isHandsUp == null) return;
+
+                            isHandsUp!.change(true);
+                          },
+                          decoration: InputDecoration(
+                              label: const Text('Password'),
+                              prefixIcon: const Icon(
+                                Icons.lock,
+                              ),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              child: const Text(
+                                'Forgot your password?',
+                                style: TextStyle(
+                                    decoration: TextDecoration.underline),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        InkWell(
+                          onTap: () {
+                            if (email_controller.text == admin_email &&
+                                password_controller.text == admin_password) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => const Main()));
+                              trigSuccess!.change(true);
+                            } else {
+                              if (isChecking != null) {
+                                isChecking!.change(false);
+                              }
+                              if (trigFail == null) return;
+
+                              trigFail!.change(true);
+                            }
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.amber,
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [Colors.blue, Colors.orange],
+                              ),
+                              // Using rive prefix to specify which LinearGradient to use
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            height: 50,
+                            width: 380,
+                            child: const Center(
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            const Text("Don't you have account?"),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            TextButton(
+                              child: const Text(
+                                'Register',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 15),
+                              ),
+                              onPressed: () {},
+                            )
+                          ],
+                        ),
+                        const Center(
+                          child: Text('Or login with:'),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.facebook,
+                                color: Colors.blue,
+                                size: 38,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/setting/setting_content.dart
@@ -0,0 +1,411 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:money_mate/login.dart';
+
+class setting_content extends StatefulWidget {
+  const setting_content({super.key});
+
+  @override
+  State<setting_content> createState() => _setting_contentState();
+}
+
+class _setting_contentState extends State<setting_content> {
+  bool is_dark = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding:
+            const EdgeInsets.only(left: 18, top: 260, right: 18, bottom: 150),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Text(
+            'Appearance',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          ),
+          Material(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+            child: Column(children: [
+              InkWell(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+                onTap: () {},
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 10.0, right: 18),
+                            child: Icon(
+                              Icons.language,
+                              color: Colors.blue,
+                              size: 26,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Language',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 16),
+                              ),
+                              Text(
+                                'English',
+                                style: TextStyle(color: Colors.grey),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.navigate_next)
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 10.0, right: 18),
+                            child: Icon(
+                              Icons.currency_exchange,
+                              color: Colors.orange,
+                              size: 26,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Currency',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 16),
+                              ),
+                              Text(
+                                'VNĐ',
+                                style: TextStyle(color: Colors.grey),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.navigate_next)
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0, right: 18),
+                          child: Icon(
+                            Icons.dark_mode,
+                            color: Colors.purple[700],
+                            size: 26,
+                          ),
+                        ),
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Dark mode',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 16),
+                            ),
+                            Text(
+                              'Automatic',
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    Switch(
+                        value: is_dark,
+                        onChanged: (bool value) {
+                          setState(() {
+                            is_dark = value;
+                          });
+                        })
+                  ],
+                ),
+              )
+            ]),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 16.0),
+            child: Text(
+              'Security & Privacy',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+          ),
+          Material(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+            child: Column(
+              children: [
+                InkWell(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)),
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.0, right: 18),
+                              child: Icon(
+                                Icons.lock,
+                                color: Colors.green,
+                                size: 26,
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Application lock',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16),
+                                ),
+                                Text(
+                                  'Use Password or FaceID to unlock app',
+                                  style: TextStyle(color: Colors.grey),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        Icon(Icons.navigate_next)
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.0, right: 18),
+                              child: Icon(
+                                Icons.privacy_tip,
+                                color: Colors.red,
+                                size: 26,
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Privacy',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16),
+                                ),
+                                Text(
+                                  'More privacy options',
+                                  style: TextStyle(color: Colors.grey),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        Icon(Icons.navigate_next)
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 16.0),
+            child: Text(
+              'Help',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+          ),
+          Material(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+            child: Column(children: [
+              InkWell(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+                onTap: () {},
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 10.0, right: 18),
+                            child: Icon(
+                              Icons.info,
+                              color: Colors.orange,
+                              size: 26,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'About',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 16),
+                              ),
+                              Text(
+                                'Learn more about Money Mate',
+                                style: TextStyle(color: Colors.grey),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.navigate_next)
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+                onTap: () {},
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 10.0, right: 18),
+                            child: Icon(
+                              Icons.feedback,
+                              size: 26,
+                              color: Colors.brown,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Send feedback',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 16),
+                              ),
+                              Text(
+                                'Let us know your experience about app',
+                                style: TextStyle(color: Colors.grey),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.navigate_next)
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 28.0),
+            child: Material(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(10),
+                onTap: () {
+                  show_curpetino_action(context);
+                },
+                child: const SizedBox(
+                  height: 50,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Sign out',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                  color: Colors.red),
+                            ),
+                          ],
+                        ),
+                      ]),
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  void show_curpetino_action(BuildContext context) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => CupertinoActionSheet(
+        title: const Text(
+          'Sign out your account?',
+          style: TextStyle(fontSize: 16),
+        ),
+        actions: <CupertinoActionSheetAction>[
+          CupertinoActionSheetAction(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => login()));
+            },
+            child: const Text('Sign out'),
+          ),
+          CupertinoActionSheetAction(
+            isDefaultAction: true,
+            isDestructiveAction: true,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+}
